b6558d2b084df7263cfc9e5d9dd10e433037e6a4
r1s3 <32917889+RIS33@users.noreply.github.com>
2019-05-17 21:01:26 -0300

    Update buscador.js

--- buscador.js
+++ buscador.js
@@ -3,14 +3,12 @@ const axios = require('axios')
 const url = `https://darksearch.io/api/search?query=${process.argv[2]}&page=${process.argv[3]}`
 
 axios.get(url)
-  .then( response =>  {
-	const dados = response['data']	
-  	const result = dados['data']
+  .then(response =>  {
+	const result = response.data.data
   	
-  	console.log(`\t\t SITES RELACIONADOS A ${process.argv[2]}\t\t\t\tPAGE ${process.argv[3]}`)
-  	result.forEach((item)=>{
+  	console.log(`\t\t SITES RELACIONADOS A ${process.argv[2]}\t\tPAGE ${process.argv[3]}`)
+  	result.forEach(item => {
   		console.log(`[*] NOME DO SITE: ${item['title']}\n[*] LINK: ${item['link']}\n\n\n`)
- 	});
-  	console.log(`\t\t SITES RELACIONADOS A ${process.argv[2]}\t\t\t\tPAGE ${process.argv[3]}`)
-  	
+ 		});
+  	console.log(`\t\t SITES RELACIONADOS A ${process.argv[2]}\t\tPAGE ${process.argv[3]}`)
   })  
